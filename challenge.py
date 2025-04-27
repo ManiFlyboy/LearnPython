13b2f4607f3a30ac9b162d95e11fb1999897c0a6
ManiFlyboy <76646517+ManiFlyboy@users.noreply.github.com>
2025-04-27 21:31:05 +0530

    TUDEDUE ASSIGNMENT1

---
+++ challenge.py
@@ -0,0 +1,25 @@
+...
+#Celsius --------> Farenheit
+c=input('Celsius:   ')
+c=float(c)
+f=(c * 9/5) + 32
+print('Farenheit:  ',f)
+...
+
+# Farenheit -------->Celsius
+f=input('Farenheit:   ')
+f=float(f)
+c=(f - 32) * 5/9
+print('Celsius:   ',c)
+...
+
+#SI Calculator
+p=input('p:  ')
+n=input('n:  ')
+r=input('r:  ')
+p=int(p)
+n=int(n)
+r=int(r)
+si=(p*n*r)/100
+print("Simple Interest is:  ",si)
+
